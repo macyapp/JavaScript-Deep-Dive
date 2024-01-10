dc53326e88f06da2221aba52a380ae3955c74be0
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2024-01-10 12:28:56 +0530

    Something about the comments

---
+++ sandbox-start/01-variables-data-types/02-comments-shortcuts/script.js
@@ -0,0 +1,39 @@
+// This is a single line of code
+
+/*
+Multi-line comment
+console.log(100);
+
+console.log("Hello, World!");
+
+console.log(20, "Hello", true);
+*/
+const x = 100;
+
+console.log(x);
+
+console.error("Alert");
+
+console.warn("Warning");
+
+console.table({
+  firstName: "Sid",
+  email: "skn@gmail.com",
+});
+
+console.group("A group");
+console.log(x);
+
+console.error("Alert");
+
+console.warn("Warning");
+
+console.table({
+  firstName: "Sid",
+  email: "skn@gmail.com",
+});
+console.groupEnd();
+
+const styles = "padding:10px; background-color:white; color:green";
+
+console.log("%cHello, World!", styles);
