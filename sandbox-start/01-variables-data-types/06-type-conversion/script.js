74738019db34c5ab945f3ec1ed17efcb29414e82
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2024-01-27 21:37:18 +0530

    Setting up lesson for DOM Manipulation

---
+++ sandbox-start/01-variables-data-types/06-type-conversion/script.js
@@ -0,0 +1,18 @@
+let amount = 'Hello';
+
+// Change string to number
+amount = parseInt(amount);
+// amount = +amount;
+// amount = Number(amount);
+
+// Change number to string
+// amount = amount.toString();
+// amount = String(amount);
+
+// Change string to decimal
+// amount=parseFloat(amount)
+
+// Convert number to boolean
+// amount = Boolean(amount);
+
+console.log(amount, typeof amount);
