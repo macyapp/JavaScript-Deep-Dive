c5f7e49f560620e837961751ab92cb85dabafb6e
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2024-02-22 13:26:39 +0530

    Update

--- sandbox-start/01-variables-data-types/07-operators/script.js
+++ sandbox-start/01-variables-data-types/07-operators/script.js
@@ -1,7 +1,9 @@
 // 1. Arithmetic Operators
-
+let x;
+x = 5 + 5;
 
 // 2. Assignment Operators
 
-
 // 3. Comparison Operators
+
+console.log(x);
--- z_Playground/main.cpp
+++ z_Playground/main.cpp
@@ -1,26 +1,15 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<iomanip>
 using namespace std;
-#define endl "\n"
-#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
-#define dbg(x) cout<<#x<<" "<<x<<endl;
-typedef long long ll;
-typedef vector<int> vi;
-typedef vector<vector<int>> vvi;
-typedef vector<long long> vll;
-typedef vector<vector<long long>> vvll;
-typedef vector<string> vs;
-
-void solve() {
-    int a,b;
-    cin>>a>>b;
-    cout<<a%b<<endl;
-}
 
 int main() {
-    fastio;
-    int t;
-    cin>>t;
-    while(t--)
-        solve();
+    cout<<setprecision(4);
+    double x=15.5683, y=34267.1;
+    cout<<x<<' '<<y<<' '<<'\n';
+    double z=1.23;
+    cout<<showpoint <<z<<'\n';
+    cout<<showpos   <<z<<'\n';
+    cout<<uppercase <<y<<'\n';
+
     return 0;
 }
