da6fc5e77fdf8db21cd4c004185d8bce532c71d2
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2024-03-20 22:53:56 +0530

    Update

--- z_Playground/main.cpp
+++ z_Playground/main.cpp
@@ -1,11 +1,43 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+#define endl "\n"
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define dbg(x) cout<<#x<<" "<<x<<endl;
+#define pb push_back
+#define ppb pop_back
+#define F first
+#define S second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define ceil(x,y) ((x+y-1)/y)
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<long long> vll;
+typedef vector<vector<long long>> vvll;
+typedef vector<string> vs;
+typedef unordered_set<int> usi;
+typedef unordered_set<long long> usll;
+typedef unordered_map<int,int> umii;
+typedef unordered_map<long long,long long> umll;
+const int mod=1e9+7;
+const int mod1=998244353;
+
+void init(vi& a) {
+    for(auto& x: a) {
+        cin>>x;
+    }
+}
 
 void solve() {
     
 }
 
 int main() {
-    solve();
+    fastio;
+    int t;
+    cin>>t;
+    while(t--)
+        solve();
     return 0;
 }
