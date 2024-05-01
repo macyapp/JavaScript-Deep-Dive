07806e6d9604c0f66be005ea6bd307aa09d75cf0
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2024-05-01 15:21:56 +0530

    ud

--- z_Playground/main.cpp
+++ z_Playground/main.cpp
@@ -1,7 +1,95 @@
 #include<iostream>
 using namespace std;
 
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* prev;
+    Node(int x)
+    :data(x), prev(nullptr), next(nullptr) {}
+};
+
+void insertTail(Node*& head, int x) {
+    Node* tmp=new Node(x);
+    if(head==nullptr) {
+        head=tmp;
+    }
+    else {
+        Node* tail=head;
+        while(tail!=nullptr && tail->next!=nullptr) {
+            tail=tail->next;
+        }
+        tail->next=tmp;
+        tmp->prev=tail;
+    }
+}
+
+void insertHead(Node*& head, int x) {
+    Node* newNode=new Node(x);
+    newNode->next=head;
+    head->prev=newNode;
+    head=newNode;
+}
+
+void print(Node* head) {
+    Node* tail=nullptr;
+    // Forward print and find tail
+    for(Node* cur=head; cur!=nullptr; cur= cur->next) {
+        cout<<cur->data<<" ";
+        tail=cur;   // Tail will be last non-null Node
+    }
+    cout<<endl;
+    // Reverse print from tail
+    for(Node* cur=tail; cur!=nullptr; cur=cur->prev) {
+        cout<<cur->data<<" ";
+    }
+    cout<<endl;
+}
+
+void deleteHead(Node*& head) {
+    Node* tmp=head;
+    if(head->next==nullptr) {
+        delete tmp;
+        head=nullptr;
+    }
+    head=head->next;
+    head->prev=nullptr;
+    tmp->next=nullptr;
+    delete tmp;
+}
+
+void deleteTail(Node*& head) {
+    Node* cur=head;
+    while(cur!=nullptr && cur->next!=nullptr) {
+        cur=cur->next;
+    }
+    Node* tmp=cur;
+    cur=cur->prev;
+    if(cur!=nullptr)
+        cur->next=nullptr;
+    delete tmp;
+}
+
+void solve() {
+    Node* head=nullptr;
+    int n;
+    cout<<"Enter the size of the list:"<<endl;
+    cin>>n;
+    cout<<"Enter "<<n<<" element"<<((n==1)?"":"s")<<endl;
+    for(int i=0; i<n; i++) {
+        int tmp;
+        cin>>tmp;
+        insertTail(head,tmp);
+    }
+    cout<<"Doubly Linked List:"<<endl;
+    print(head);
+    insertHead(head,1);
+    cout<<"Doubly Linked List:"<<endl;
+    print(head);
+}
+
 int main() {
-    cout<<"Hello, World!"<<endl;
+    solve();
     return 0;
 }
---
+++ z_Playground/play.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define dbg(x) cout<<#x<<": "<<x<<endl;
+#define pb push_back
+#define ppb pop_back
+#define F first
+#define S second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define ceil(x,y) ((x+y-1)/y)
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<long long> vll;
+typedef vector<vector<long long>> vvll;
+typedef vector<string> vs;
+typedef unordered_set<int> usi;
+typedef unordered_set<char> usc;
+typedef unordered_set<long long> usll;
+typedef unordered_map<int,int> umii;
+typedef unordered_map<long long,long long> umll;
+const int mod=1e9+7;
+const int mod1=998244353;
+
+void init(vi& a) {
+    for(auto& x: a) {
+        cin>>x;
+    }
+}
+
+void solve() {
+    int n;
+    cin>>n;
+    vi a(n);
+    init(a);
+    int minCount=3;
+    for(int i=0; i<n; i++) {
+        if(a[a[i]-1]==i+1)
+            minCount=2;
+    }
+    cout<<minCount<<endl;
+}
+
+int main() {
+    fastio;
+    int t;
+    cin>>t;
+    while(t--)
+        solve();
+    return 0;
+}
